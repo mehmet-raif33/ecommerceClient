afb2ae2146d21a53768dd741f30395bed860564f
mehmetraif12 <mehmetraif33@gmail.com>
2024-06-12 00:29:16 +0300

    makedAnimetion

--- src/Pages/MainLayer.tsx
+++ src/Pages/MainLayer.tsx
@@ -1,27 +1,59 @@
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
 import { useState } from "react"
-import { useDispatch, useSelector } from 'react-redux'
-import store from "../redux/Store"
-import { addTodo, removeTodo, deleteAllTodos } from './ToDoList/ToDoListSlice'
+import { useSelector , useDispatch } from "react-redux"
+import { RootState } from "../redux/Store"
+import { addTodo, removeTodo } from '../redux/ToDoList/ToDoListSlice'
+import { Todo } from "../redux/ToDoList/ToDoListSlice"
+import { AnimatePresence , motion } from "framer-motion"
+
+const container = {
+  hidden: {
+
+  },
+  visible: {
+
+  }
+}
+
+const item = {
+  hidden: {
+
+  },
+  visible: {
+    
+  }
+}
 
 export const MainLayer = () => {
+  
 
-  const myData = useSelector(state => state.todoSlice.value)
+  const dispatch = useDispatch();
+  const [inputData, setInputData] = useState<string>('')
 
-  const [liste, setListe] = useState(['selam'])
-
+  const myData = useSelector( (state: RootState) => state.todos.todos)
+  
+  console.log(myData)
   return (
     <div className="bg-sky-200">
         <Navbar />
         <div className="h-[800px]">
           <div className="p-3 bg-stone-200 flex justify-center">
             <input
+            onChange={(e) => {
+              setInputData(e.target.value)
+            }}
+            value={inputData} 
             className="p-2 outline-none rounded-lg"
             type="search" placeholder="Enter a Person" />
             <button
             className="p-2 bg-yellow-300 rounded-md pt-1 mx-3 hover:scale-105 transition-all hover:bg-yellow-400"
-            
+            onClick={() => {
+              if (inputData.trim()) {  // inputData boş değilse ekle
+                dispatch(addTodo(inputData));
+                setInputData('');  // input'u temizle
+              }
+            }}
             >
               Enter
             </button>
@@ -29,15 +61,32 @@ export const MainLayer = () => {
           <div
           className="flex flex-col bg-sky-500 p-5 text-white text-lg"
           >
-            {
-              liste.map((a: String) => {
-                <div className="mb-5">
-                  <h2>
-                    {a}
-                  </h2>
-                </div>
-              })
-            }
+            <AnimatePresence >
+              {
+                myData.map((a: Todo) => (
+                  <motion.div 
+                  initial={{ opacity: 0, x:-50 }}
+                  animate={{ opacity: 1, x:0 }}
+                  exit={{ opacity: 0, x: 50 }} 
+                  className="mb-5 flex flex-row justify-between" 
+                  key={a.id}>
+
+                    <h2 className="bg-slate-200 p-3 text-black rounded-lg">
+                      {a.text}
+                    </h2>
+
+                    <button
+                      className="ml-2 bg-red-500 p-1 rounded"
+                      onClick={() => dispatch(removeTodo(a.id))}
+                    >
+                      Delete
+                    </button>
+
+                  </motion.div>
+                ))
+              }
+            </AnimatePresence>
+            
             
           </div>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,24 @@ export const Navbar = () => {
         
         <div className="flex flex-col justify-center">
           <a href="" className="flex justify-center">
-            <img src="../images/website_logo.jpg" alt="asdasd"/>
+            <img 
+            src="public/images/website_logo.jpg" 
+            alt="asdasd"
+            className="w-[250px]"
+            />
           </a>
         </div>
 
-        <div>
-          <ul>
-            <li>
+        <div className="">
+          <ul className="flex flex-row m-3 w-[100%]">
+            <li className="mr-3">
               <a href="/homepage" >
-                <img width="30" height="30" src="https://img.icons8.com/offices/30/home.png" alt="home"/>
+                <img width="40" height="40" src="https://img.icons8.com/offices/30/home.png" alt="home"/>
+              </a>
+            </li>
+            <li>
+              <a href="/profile" >
+                <img width="40" height="40" src="https://img.icons8.com/fluency/96/name--v1.png" alt="home"/>
               </a>
             </li>
           </ul>
