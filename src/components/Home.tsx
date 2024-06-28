8d7e36b738d3d28baffa1ddc3775e90b92c05011
mehmetraif12 <mehmetraif33@gmail.com>
2024-06-28 04:54:15 +0300

    addedNewViideo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
     <div className="grid grid-cols-12">
       <div className="grid col-span-7 bg-slate-50 p-1">
         <video width="100%" controls className="mt-2 mx-2">
-          <source src="videos/5bfl15.mp4" type="video/mp4" />
+          <source src="videos/newYork.mp4" type="video/mp4" />
         </video>
         <label className="text-2xl m-3">
           Killer is shooting the other people!!
