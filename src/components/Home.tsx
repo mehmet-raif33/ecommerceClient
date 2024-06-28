4b3bda66707c32521cf52328755f90973e18cd1e
mehmetraif12 <mehmetraif33@gmail.com>
2024-06-28 04:43:51 +0300

    ididit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,18 +2,24 @@
 const Home = () => {
   return (
     <div className="grid grid-cols-12">
-      <div className="grid col-span-10 bg-slate-50 p-2">
-        <video width="100%" controls>
+      <div className="grid col-span-7 bg-slate-50 p-1">
+        <video width="100%" controls className="mt-2 mx-2">
           <source src="videos/5bfl15.mp4" type="video/mp4" />
         </video>
         <label className="text-2xl m-3">
           Killer is shooting the other people!!
         </label>
       </div>
-      <div className="grid col-span-2">
-        <h1>
-          KSJDSALKsadsdf
-        </h1>
+      <div className="grid col-span-5 p-3 bg-red-700 text-white rounded-md m-3 justify-center items-center" >
+        <div className="w-[170px] h-[200px]">
+          <h1 className="text-center text-lg">
+            ATTENTION!!
+          </h1>
+          <p className="text-sm italic">
+            This video contains very bloody contents! If you have any health problem, you must leave this page and never come back! We are not take a responsibilities that can be happen!
+          </p>          
+        </div>
+
       </div>
 
     </div>
