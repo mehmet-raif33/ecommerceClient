34892c00c532276e395145f052c2ca0f80146740
mehmetraif12 <mehmetraif33@gmail.com>
2024-06-28 04:15:43 +0300

    addedNewFeatures

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,22 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="grid grid-cols-12">
+      <div className="grid col-span-10 bg-slate-50 p-2">
+        <video width="100%" controls>
+          <source src="public/5bfl15.mp4" type="video/mp4" />
+        </video>
+        <label className="text-2xl m-3">
+          Killer is shooting the other people!!
+        </label>
+      </div>
+      <div className="grid col-span-2">
+        <h1>
+          KSJDSALK
+        </h1>
+      </div>
+
+    </div>
   )
 }
 
