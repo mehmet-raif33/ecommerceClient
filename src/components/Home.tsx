494b69dbd3c8a81bbda731d143cba72f9f2e5ce4
mehmetraif12 <mehmetraif33@gmail.com>
2024-06-28 04:24:12 +0300

    deneme2

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
     <div className="grid grid-cols-12">
       <div className="grid col-span-10 bg-slate-50 p-2">
         <video width="100%" controls>
-          <source src="public/5bfl15.mp4" type="video/mp4" />
+          <source src="videos/5bfl15.mp4" type="video/mp4" />
         </video>
         <label className="text-2xl m-3">
           Killer is shooting the other people!!
@@ -12,7 +12,7 @@ const Home = () => {
       </div>
       <div className="grid col-span-2">
         <h1>
-          KSJDSALKsad
+          KSJDSALKsadsdf
         </h1>
       </div>
 
